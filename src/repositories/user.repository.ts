172f863cd8b4bad01071c8e2b5e93c70948c65cd
Erick Paiva <123skatesk8@gmail.com>
2022-06-09 09:45:10 -0400

    feat: I created the user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,21 @@
+import { Repository } from "typeorm";
+import { AppDataSource } from "../data-source";
+import { User } from "../entities/user.entity";
+
+class UserRepo {
+  private ormRepo: Repository<User>;
+
+  constructor() {
+    this.ormRepo = AppDataSource.getRepository(User);
+  }
+
+  save = async (user: Partial<User>) => await this.ormRepo.save(user);
+  
+  all = async () => await this.ormRepo.find();
+
+  findOne = async (payload: object) => {
+    return await this.ormRepo.findOneBy({ ...payload });
+  };
+}
+
+export default new UserRepo();
