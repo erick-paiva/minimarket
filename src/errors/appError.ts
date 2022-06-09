27ef8dbeec5459254a94a4d25b706c0b72d14dbf
Erick Paiva <87384504+erick-paiva@users.noreply.github.com>
2022-06-09 09:45:44 -0400

    Feature/login user (#9)
    * refactor: remove a key message
    
    * feat: I created the global user error class
    
    * feat: I created the user login and returned the token
    
    * feat: I created the user repository

--- src/errors/appError.ts
+++ src/errors/appError.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+type TMessage = string | Record<string, any> | object;
 export class AppError extends Error {
   statusCode: number;
 
@@ -8,6 +9,17 @@ export class AppError extends Error {
     this.message = message;
   }
 }
+
+export class UserError {
+  public statusCode: number;
+  public message: TMessage;
+
+  constructor(statusCode: number, message: TMessage) {
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
 export const handleError = (err: any, res: Response) => {
   const { statusCode, message } = err;
 
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,7 +3,22 @@ import { Request } from "express";
 import { serializedCreateUserSchema } from "../schemas";
 import { AssertsShape } from "yup/lib/object";
 import { AppDataSource } from "../data-source";
+import userRepository from "../repositories/user.repository";
+import { UserError } from "../errors/appError";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "dotenv";
 
+config();
+interface ILogin {
+  status: number;
+  message: object | string;
+}
+
+interface ILoginData {
+  email: string;
+  password: string;
+}
 class userService {
   createUser = async ({ validated }: Request): Promise<AssertsShape<any>> => {
     const userRepository = AppDataSource.getRepository(User);
@@ -13,8 +28,31 @@ class userService {
       stripUnknown: true,
     });
   };
-  loginUser = () => {
-    return { status: 200, message: "login" };
+  loginUser = async (userData: ILoginData): Promise<ILogin> => {
+    const { email, password } = userData as ILoginData;
+
+    const user = (await userRepository.findOne({ email })) as User | any;
+
+    if (!user) {
+      throw new UserError(404, "User not found");
+    }
+
+    const isValid = await compare(password, user?.password);
+
+    if (!isValid) {
+      throw new UserError(401, "Email or password is incorrect");
+    }
+
+    const token = sign({ id: user.email }, process.env.SECRET_KEY as string, {
+      expiresIn: process.env.EXPIRES_IN as string,
+    });
+
+    return {
+      status: 200,
+      message: {
+        token,
+      },
+    };
   };
 }
 
