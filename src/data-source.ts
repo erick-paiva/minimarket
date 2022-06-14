765b46e4cb94692508a1c1787308ee08dafe0c5d
Erick Paiva <123skatesk8@gmail.com>
2022-06-14 14:37:23 -0400

    make it better

--- src/data-source.ts
+++ src/data-source.ts
@@ -17,12 +17,6 @@ export const AppDataSource =
         type: "postgres",
         url: process.env.DATABASE_URL,
         logging: false,
-        entities:
-          process.env.NODE_ENV === "production"
-            ? ["dist/entities/*.js"]
-            : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
-            ? ["dist/migrations/*.js"]
-            : ["src/migrations/*.ts"],
+        entities: [path.join(__dirname, "./entities/**/*.{js,ts}")],
+        migrations: [path.join(__dirname, "./migrations/**/*.{js,ts}")],
       });
