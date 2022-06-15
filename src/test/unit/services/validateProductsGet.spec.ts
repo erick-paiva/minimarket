27853d3e3d2655a128317f21d460311b1d642a3a
Bruno Tetzner <brunoltetzner@gmail.com>
2022-06-15 20:05:07 -0300

    feat: added verify test to valid establishment owner

--- src/test/unit/services/validateProductsGet.spec.ts
+++ src/test/unit/services/validateProductsGet.spec.ts
@@ -66,7 +66,7 @@ describe("Get products test", () => {
   };
 
   let establishmentId = "";
-  test("Should be able to get all establishments products", async () => {
+  test("Should be able to get all establishment products", async () => {
     const createUserResponse = await request(app)
       .post("/api/signup")
       .send(userData)
@@ -109,5 +109,76 @@ describe("Get products test", () => {
       .set("Authorization", "Bearer " + token(true));
     expect(productResponse.status).toBe(404);
     expect(productResponse.body).toHaveProperty("error");
+  });
+
+  test("Should be error 401 if user isn't the establishment owner", async () => {
+    const newUserData = {
+      name: faker.name.firstName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      contact: faker.phone.imei(),
+      password: faker.mersenne.rand().toString(),
+      avatar: faker.image.avatar(),
+    };
+    const createUserResponse = await request(app)
+      .post("/api/signup")
+      .send(newUserData)
+      .set("Authorization", "Bearer " + token(true));
+
+    const signinResponse = await request(app)
+      .post("/api/signin")
+      .send({ email: newUserData.email, password: newUserData.password });
+
+    const unauthorizedUserData = {
+      name: faker.name.firstName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      contact: faker.phone.imei(),
+      password: faker.mersenne.rand().toString(),
+      avatar: faker.image.avatar(),
+    };
+    await request(app)
+      .post("/api/signup")
+      .send(unauthorizedUserData)
+      .set("Authorization", "Bearer " + token(true));
+
+    const signinResponseUnauthorizedUser = await request(app)
+      .post("/api/signin")
+      .send({
+        email: unauthorizedUserData.email,
+        password: unauthorizedUserData.password,
+      });
+
+    const newEstablishmentData = {
+      name: faker.company.companyName(),
+      cnpj: faker.mersenne.rand().toString(),
+      address: {
+        street: faker.address.street(),
+        number: faker.mersenne.rand(),
+        zipCode: faker.address.zipCode(),
+        district: faker.address.cityName(),
+      },
+      contact: faker.phone.imei(),
+      urlLogo: faker.image.imageUrl(),
+      userId: createUserResponse.body.id,
+    };
+
+    const tokenUser = signinResponse.body.token;
+    const tokenUnathorizedUser = signinResponseUnauthorizedUser.body.token;
+
+    const establishmentResponse = await request(app)
+      .post("/api/establishment")
+      .send(newEstablishmentData)
+      .set("Authorization", `Bearer ${tokenUser}`);
+
+    establishmentId = establishmentResponse.body.id;
+    productData.establishmentId = establishmentId;
+    await request(app)
+      .post("/api/product")
+      .send(productData)
+      .set("Authorization", "Bearer " + tokenUser);
+
+    const getProductsResponse = await request(app)
+      .get(`/api/product/${establishmentId}`)
+      .set("Authorization", "Bearer " + tokenUnathorizedUser);
+    expect(getProductsResponse.status).toBe(401);
   });
 });
