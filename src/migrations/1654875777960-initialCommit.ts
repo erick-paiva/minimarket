547834ded0bb482a933a30607c8fd4a27f11b817
Gabriel Amaral <88339036+Gabriel-dsAmaral@users.noreply.github.com>
2022-06-20 10:27:52 -0300

    feat: mocked categories added in migration (#28)

--- src/migrations/1654875777960-initialCommit.ts
+++ src/migrations/1654875777960-initialCommit.ts
@@ -90,6 +90,25 @@ export class initialCommit1654875777960 implements MigrationInterface {
       )}','https://images.uncyc.org/pt/1/18/Dobby_HarryPotter.jpg',true)
           `
     );
+    await queryRunner.query(
+      `
+            INSERT INTO "categories" ("name", "image")
+            VALUES ('Hortifruti','https://lorensonline.com.br/wp-content/uploads/2019/06/Lorens-Crescimento-do-Hortifruti-no-Brasil-legumes-sortidos-2.jpg'),
+            ('Produtos de limpeza','https://cglimpeza.com.br/wp-content/uploads/2018/12/2019-01-itens-indispensaveis-no-estoque-de-produtos-para-limpeza-1.jpg'),
+            ('Higiene e perfumaria','https://site.abcfarma.org.br/wp-content/uploads/2019/05/loja-de-cosmeticos-virtual-como-montar-1.jpg'),
+            ('Congelados','https://boaforma.abril.com.br/wp-content/uploads/sites/2/2018/07/thinkstockphotos-626104514.jpg'),
+            ('Carnes','https://www.saboravida.com.br/wp-content/uploads/2019/12/veja-como-preparar-diferentes-cortes-de-carnes-800x445.jpg'),
+            ('Latas e conservas','https://www.bonduelle.com.br/images/produtos/latas-conservas.png'),
+            ('Farinhas e grãos','https://nutritotal.com.br/publico-geral/wp-content/uploads/sites/2/2019/09/shutterstock_272086718-1.jpg'),
+            ('Frios','https://www.sabornamesa.com.br/media/k2/items/cache/01db144526716df630e705de85c35be7_XL.jpg'),
+            ('Legumes','https://s2.glbimg.com/wpd84CZmDM9Wzn2sWdLoQdHoB6s=/0x0:750x500/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2020/h/I/6O16OJRB2sUMIDqtxlmQ/alimentos-crus.jpg'),
+            ('Bebidas','https://www.galaxcms.com.br/imgs_crud_comum/1801/Bebidas-acucaradas-incluindo--20190711142328.jpg'),
+            ('Padaria','https://730370.smushcdn.com/1861587/wp-content/uploads/2021/09/balcao-de-padaria-1024x576.jpg?lossy=1&strip=1&webp=1'),
+            ('Açougue','https://blog.atau.com.br/wp-content/uploads/2018/08/Dicas-para-atrair-mais-clientes-para-o-seu-acougue-atau-1900x1069.jpg'),
+            ('Energéticos','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRezTO51dI9vlvGVC_l8AYRjNRSc7dVxEN80Q&usqp=CAU'),
+            ('Biscoitos e Chocolates','https://www.marolacomcarambola.com.br/wp-content/uploads/2016/10/receita-de-biscoitos-confeitados-com-chocolate-2.jpg')
+          `
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
