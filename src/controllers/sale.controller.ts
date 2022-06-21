2ff5c4d752d244693b107224ad7c23abde98afc4
Erick Paiva <123skatesk8@gmail.com>
2022-06-21 19:57:59 -0400

    feat: create sale update controller

--- src/controllers/sale.controller.ts
+++ src/controllers/sale.controller.ts
@@ -7,6 +7,18 @@ class SaleController {
     const sale = await SaleService.createSale(req);
     return res.status(201).json(sale);
   };
+
+  patchSale = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const { payment } = req.body;
+      const { status, message } = await SaleService.patchSale(id, payment);
+      return res.status(status).json({ message: message });
+    } catch (err) {
+      return handleError(err, res);
+    }
+  };
+
   getSales = async (req: Request, res: Response) => {
     const { establishmentId } = req.params;
 
