cd85a3f38a8253602041e74f599911d0efd4d4a0
Erick Paiva <123skatesk8@gmail.com>
2022-06-21 20:03:42 -0400

    feat: I added the validate schema in the sale update route

--- src/routes/sale.routes.ts
+++ src/routes/sale.routes.ts
@@ -2,7 +2,10 @@ import "express-async-errors";
 import { Router } from "express";
 import SaleController from "../controllers/sale.controller";
 import { validadeSchema, validateToken } from "../middlewares";
-import { createSaleSchema } from "../schemas/sale/create.schema";
+import {
+  createSaleSchema,
+  updateSaleSchema,
+} from "../schemas/sale/create.schema";
 const saleRouter = Router();
 
 saleRouter.post(
@@ -11,7 +14,11 @@ saleRouter.post(
   validadeSchema(createSaleSchema),
   SaleController.createSale
 );
-saleRouter.patch("/sale/:id", SaleController.patchSale);
+saleRouter.patch(
+  "/sale/:id",
+  validadeSchema(updateSaleSchema),
+  SaleController.patchSale
+);
 
 saleRouter.get("/sale/:establishmentId", SaleController.getSales);
 export default saleRouter;
